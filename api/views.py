7802aa80876802e04f3e00de112950265c38ce0b
Adisak Khongsuk <Adisak.General@gmail.com>
2025-01-14 22:02:07 +0700

    Update Table Database

--- api/views.py
+++ api/views.py
@@ -2,10 +2,9 @@
 from rest_framework import viewsets, generics, permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework import status
-from .models import Intern, Education, Training, WorkExperience
+from .models import Intern, Education, Training, WorkExperience, User
 from .serializers import InternSerializer, EducationSerializer, TrainingSerializer, WorkExperienceSerializer, RegisterSerializer
 
 # Create your views here.
--- dekend/settings.py
+++ dekend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'drf_yasg',
     'api',
@@ -192,3 +193,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# กำหนดชื่อตารางสำหรับตรวจสอบสิทธิ์
+AUTH_USER_MODEL = 'api.User'

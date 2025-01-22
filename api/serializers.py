04f38793e9e53c37353f65d975e59a29e31499ce
Adisak Khongsuk <Adisak.General@gmail.com>
2025-01-22 22:46:17 +0700

    Fix fetch login

--- api/serializers.py
+++ api/serializers.py
@@ -69,3 +69,9 @@ class LogoutView(APIView):
             return Response(status=204)
         except Exception:
             return Response(status=400)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'tel')
+        read_only_fields = ('id',)
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import InternViewSet, EducationViewSet, TrainingViewSet, WorkExperienceViewSet, RegisterView, LogoutView
+from .views import InternViewSet, EducationViewSet, TrainingViewSet, WorkExperienceViewSet,RegisterView, LogoutView, LoginView
 
 router = DefaultRouter()
 router.register(r'interns', InternViewSet)
@@ -12,10 +11,10 @@ router.register(r'educations', EducationViewSet)
 router.register(r'trainings', TrainingViewSet)
 router.register(r'work-experiences', WorkExperienceViewSet)
 
-urlpatterns =[
+urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='auth_register'),
-    path('auth/login/', TokenObtainPairView.as_view(), name='auth_login'),
+    path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='auth_refresh'),
-    path('', include(router.urls)), #เพิ่มเส้นทางสำหรับการเข้าถึงช้อมูลทั้งหมดของ router
+    path('', include(router.urls)), # เพิ่มเส้นทางสำหรับการเข้าถึงข้อมูลทั้งหมดของ router
 ]
